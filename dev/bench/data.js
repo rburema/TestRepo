a144462975070afb4e4720f76496c0efd761a5f2
github-action-benchmark <github@users.noreply.github.com>
2023-03-19 20:43:50 +0000

    add My Project BLEH Benchmark (customBiggerIsBetter) benchmark result for 46b6fd9f4d87cc75f3e248d0f9d90c4b64da8f97

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1679258630124,
+  "repoUrl": "https://github.com/rburema/TestRepo",
+  "entries": {
+    "My Project BLEH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "r.burema@ultimaker.com",
+            "name": "Remco Burema",
+            "username": "rburema"
+          },
+          "committer": {
+            "email": "r.burema@ultimaker.com",
+            "name": "Remco Burema",
+            "username": "rburema"
+          },
+          "distinct": true,
+          "id": "46b6fd9f4d87cc75f3e248d0f9d90c4b64da8f97",
+          "message": "skjfnslkfnslf",
+          "timestamp": "2023-03-19T21:43:22+01:00",
+          "tree_id": "22a0b22a98d5368507a30bfab4c1e07a5f61c31a",
+          "url": "https://github.com/rburema/TestRepo/commit/46b6fd9f4d87cc75f3e248d0f9d90c4b64da8f97"
+        },
+        "date": 1679258628239,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Aaa",
+            "value": 10.5,
+            "unit": "Squerps"
+          },
+          {
+            "name": "Bbb",
+            "value": 44,
+            "unit": "Blorf"
+          }
+        ]
+      }
+    ]
+  }
+}
