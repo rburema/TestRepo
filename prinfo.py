b05aff73aaed0b83cc708e488e8796343f8f6af0
Remco Burema <r.burema@ultimaker.com>
2023-04-26 17:04:06 +0200

    :fox::fox:

--- prinfo.py
+++ prinfo.py
@@ -2,7 +2,10 @@ import sys
 import json
 
 print(sys.argv[1])
-data = json.load(sys.argv[1])
+
+data = None
+with open(sys.argv[1]) as f:
+    data = json.load(f)
 
 for pr in data:
 	print(pr["id"])
