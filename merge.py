bc4da1fd0e3a05a90e32f79cb92fed6d55928008
Remco Burema <r.burema@ultimaker.com>
2023-05-24 09:03:53 +0200

    ::::;;;;::::

--- merge.py
+++ merge.py
@@ -2,29 +2,31 @@ from subprocess import run
 def rrun(cmd):
     res = run(cmd.split(" "), capture_output=True, text=True)
     if res.returncode != 0:
-        err = f"failed to run {cmd} because {res.stderr if res.stderr else res.stdout}"
+        err = f"failed to run '{cmd}' because '{res.stderr if res.stderr else res.stdout}'"
         print(err)
-        raise Exception()
+        raise Exception(err)
     return res.stdout
 def getBranches():
     raw_branches=rrun("git branch -r --format=%(refname:short)")
     return [x.split("/")[1] for x in raw_branches.split("\n") if x]
 print(rrun("git --version"))
 rrun("git config pull.rebase false")
+rrun("git fetch --all")
 for branch in getBranches():
     # TODO: Also for the latest version branch (like 5.2.2, 5.4.0, etc.) 
-    if branch == "brain" or branch == "HEAD": # or not branch.startsWith("CURA-"):
+    if branch == "main" or branch == "HEAD" or not branch.startswith("CURA-"):
         continue
-    #try:
-    #    ticket_no = int(branch.split("-")[1])
-    #except:
-    #    continue
-    #if ticket_no < 10000:
-    #    continue
-    
+    try:
+        ticket_no = int(branch.split("-")[1])
+    except:
+        continue
+    if ticket_no < 9000:
+        continue
     print(f"branch: {branch}")
     hash_pre=""
     try:
+        rrun("git checkout main")
+        rrun("git pull")
         rrun(f"git checkout {branch}")
         rrun("git pull")
         hash_pre = rrun("git rev-parse HEAD")
