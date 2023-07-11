782e1a19832e51ba7c060ac85465515a51f42c59
Remco Burema <r.burema@ultimaker.com>
2023-07-11 19:28:48 +0200

    [[[[[[

--- main.cpp
+++ main.cpp
@@ -12,14 +12,17 @@ int main()
         std::cout << c;
     std::cout << '\n';
 
-    const std::vector<std::vector<int>> v{ { 1, 2 }, { 3, 4, 5 }, { 6 }, { 7, 8, 9 } };
+
+
+    const std::vector<std::vector<int>> v{ { 1,2 }, { 3, 4, 5 }, { 6 }, { 7, 8, 9 } };
     const std::vector<std::vector<std::vector<int>>> w{ v, v, v };
 
     auto jv = std::ranges::join_view(w);
     for (auto const e : jv)
     {
         std::cout << '\n';
-        for (auto const ee : e)
+        for (auto const ee :e
+		)
         {
             std::cout << ee << ' ';
         }
