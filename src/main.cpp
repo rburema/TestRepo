07ebf52ce01e8cde456439130dd11ad0fbe5002c
Remco Burema <r.burema@ultimaker.com>
2023-07-12 15:15:04 +0200

    grttrg

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,5 @@
-#include <iostream>
-#include <ranges>
-#include <string_view>
-#include <vector>
-
 int main()
 {
-    using namespace std::literals;
-
-    const auto bits = { "https:"sv, "//"sv, "cppreference"sv, "."sv, "com"sv };
-    for (char const c : bits | std::views::join)
-        std::cout << c;
-    std::cout << '\n';
-
-    const std::vector<std::vector<int>> v{ { 1, 2 }, { 3, 4, 5 }, { 6 }, { 7, 8, 9 } };
-    const std::vector<std::vector<std::vector<int>>> w{ v, v, v };
-
-    auto jv = std::ranges::join_view(w);
-    for (auto const e : jv)
-    {
-        std::cout << '\n';
-        for (auto const ee : e)
-        {
-            std::cout << ee << ' ';
-        }
-    }
-    std::cout << '\n';
+	bool a = false;
+    return 00;
 }
